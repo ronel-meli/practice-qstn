96347d34ee1a127026fb2757ef817943172a41bb
ronel-meli <ronelmeli24@gmail.com>
2025-04-26 13:25:40 +0300

    Update num2_even_or_odd.py

--- num2_even_or_odd.py
+++ num2_even_or_odd.py
@@ -1,1 +1,11 @@
+#a function to check if a number is odd or even
+def even_or_odd(n):
+  #if else statement to check if the number has a reminder or not to check if it is odd or even
+  if n%2==o:
+    return "Even"
+  else:
+    return "Odd"
+
+print(even_or_odd(8)) # output is Even 
+
 
