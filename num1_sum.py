ee4a2bec343df57bb8631c92686ad3c0f253fd51
ronel-meli <ronelmeli24@gmail.com>
2025-04-26 12:59:35 +0300

    Update num1_sum.py

--- num1_sum.py
+++ num1_sum.py
@@ -1,1 +1,12 @@
+#create function to calculate sum of numbers on a list
+def calculate_sum(numbers):
+  #start total to 0
+  sum=O
+  #for loop for the numbers of the list
+for num in numbers:
+  sum+=num # add each number of the list to the sum
+  return sum
 
+print(calculate_sum([5,6,7,8]))
+#output should be 26
+  
